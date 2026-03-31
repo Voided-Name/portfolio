d09b386606ec19509b8dcd5251a24681855644ac
Faustino25 <127408104+Voided-Name@users.noreply.github.com>
2026-04-01 06:23:54 +0800

    profile picture in home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.naeberber',
   vite: {
     plugins: [tailwindcss()],
   },
